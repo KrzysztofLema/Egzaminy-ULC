888c5f42886aefc8d3c8b81fe714c3abc8cb950e
Krzysztof Lema <lema10@o2.pl>
2024-07-12 17:49:51 +0200

    initial commit

---
+++ Apps/Main/Egzaminy-ULC/EgzaminyULCApp.swift
@@ -0,0 +1,11 @@
+import AppFeature
+import SwiftUI
+
+@main
+struct EgzaminyULCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
---
+++ Packages/Sources/HomeFeature/Filters/Filter.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum Filter: AutoKind {
+    case contains(String)
+    case prefix(String)
+    case suffix(String)
+}
