ae4f0f9784fd1b19b833f3dbb4264d52f6ceb75b
Krzysztof Lema <lema10@o2.pl>
2024-08-03 13:44:34 +0200

    build(ULC-5): added composable architecture to project.

--- Apps/Main/ci_scripts/ci_post_clone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Starting ci_post_clone.sh script"
-defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
-
-defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES.
-
